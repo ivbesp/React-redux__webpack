c8ae7e4844d8162a687346eb6e52774c6366f8d6
ivbesp <iv.besp@yandex.ru>
2018-03-12 16:14:42 +0300

    добавлен стиль к точке входа

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
+import './../scss/style.scss';
 
 //Reducers
 import * as reducers from './reducers';
